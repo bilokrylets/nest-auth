a947777976064cc8231387555604136bb1bf76fe
Yaroslav Bilokrylets <4believy@gmail.com>
2023-08-25 12:53:55 +0300

    feat: users routes with guards

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,10 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from 'prisma/prisma.service';
+import { Request } from 'express';
 
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
-  async getMyUser(id: string) {}
+  async getMyUser(id: string, req: Request) {
+    const user = await this.prisma.user.findUnique({ where: { id } });
+
+    if (!user) {
+      throw new NotFoundException();
+    }
+
+    const decodedUser = req.user as { id: string; email: string };
+
+    if (user.id !== decodedUser.id) {
+      throw new ForbiddenException();
+    }
+
+    delete user.hashedPassword;
+
+    return { user };
+  }
 
   async getUsers() {
     return await this.prisma.user.findMany({
